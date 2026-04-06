5f3974802c7351bbb33764884f8a6bbd268fed16
jereseka-ux <jereseka@gmail.com>
2026-04-06 13:32:24 +0200

    Update approve.js

--- api/approve.js
+++ api/approve.js
@@ -1,19 +1,6 @@
 export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).json({ error: 'Method Not Allowed' });
-  const { paymentId } = req.body;
-  const apiKey = "Dzp2ztww1ulrhk3h0ml182jkeonkpgellgj8ccjypsncntbasjrkmubk8ex2a1zd"; 
-
-  try {
-    const response = await fetch(`https://api.minepi.com/v2/payments/${paymentId}/approve`, {
-      method: 'POST',
-      headers: {
-        'Authorization': `Key ${apiKey}`,
-        'Content-Type': 'application/json'
-      }
-    });
-    const data = await response.json();
-    return res.status(200).json(data);
-  } catch (error) {
-    return res.status(500).json({ error: "Approve Failed" });
-  }
+  return res.status(200).json({ 
+    message: "Server is working",
+    keyExists: true 
+  });
 }

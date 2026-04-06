7a944f89e28a797bdf01784364fdf5deb4578c08
jereseka-ux <jereseka@gmail.com>
2026-04-06 13:05:11 +0200

    Update approve.js

--- api/approve.js
+++ api/approve.js
@@ -1,12 +1,7 @@
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method Not Allowed' });
-  }
-
+  if (req.method !== 'POST') return res.status(405).json({ error: 'Method Not Allowed' });
   const { paymentId } = req.body;
-  
-  // أهم حاجة: حطي الـ Secret Key بتاعك هنا بين الـ ""
-  const apiKey = "اكتبي_هنا_مفتاح_الـ_API_الخاص_بكي";
+  const apiKey = "Dzp2ztww1ulrhk3h0ml182jkeonkpgellgj8ccjypsncntbasjrkmubk8ex2a1zd"; 
 
   try {
     const response = await fetch(`https://api.minepi.com/v2/payments/${paymentId}/approve`, {
@@ -16,15 +11,9 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json'
       }
     });
-
-    if (response.ok) {
-      const data = await response.json();
-      return res.status(200).json(data);
-    } else {
-      const errorData = await response.json();
-      return res.status(response.status).json(errorData);
-    }
+    const data = await response.json();
+    return res.status(200).json(data);
   } catch (error) {
-    return res.status(500).json({ error: "Internal Server Error" });
+    return res.status(500).json({ error: "Approve Failed" });
   }
 }

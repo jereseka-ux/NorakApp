4e43376509057e902ad26035127a2472d066ec5a
jereseka-ux <jereseka@gmail.com>
2026-04-06 14:03:03 +0200

    Update complete.js

--- api/complete.js
+++ api/complete.js
@@ -13,7 +13,6 @@ export default async function handler(req, res) {
       body: JSON.stringify({ txid })
     });
     const data = await response.json();
-    console.log("Pi Complete Response:", data);
     return res.status(200).json(data);
   } catch (error) {
     return res.status(500).json({ error: "Complete Failed" });

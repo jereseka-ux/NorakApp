cb21ab4d467396f1c2d412c2366431a820490aff
jereseka-ux <jereseka@gmail.com>
2026-04-07 16:58:00 +0200

    Update approve.js

--- api/approve.js
+++ api/approve.js
@@ -1,7 +1,8 @@
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method Not Allowed' });
+
   const { paymentId } = req.body;
-  const apiKey = "Dzp2ztww1ulrhk3h0ml182jkeonkpgellgj8ccjypsncntbasjrkmubk8ex2a1zd"; 
+  const apiKey = process.env.PI_API_KEY; // بيقرأ من ڤيرسل مباشرة
 
   try {
     const response = await fetch(`https://api.minepi.com/v2/payments/${paymentId}/approve`, {
@@ -11,9 +12,8 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json'
       }
     });
+
     const data = await response.json();
-    
-    // تأكدي إن السعر هنا 0.5 عشان يوافق
     return res.status(200).json(data);
   } catch (error) {
     return res.status(500).json({ error: "Approve Failed" });
